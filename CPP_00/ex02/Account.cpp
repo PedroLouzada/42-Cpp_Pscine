c49886268710624c587f06cdc28af49184c4a86a
Pedro Louzada <pedrolouzada17@gmail.com>
2026-03-18 22:08:40 +0000

    started bonus

---
+++ CPP_00/ex02/Account.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 21:50:22 by pedro             #+#    #+#             */
+/*   Updated: 2026/03/18 22:04:18 by pedro            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+	static int	getNbAccounts( void );
+	// static int	getTotalAmount( void );
+	// static int	getNbDeposits( void );
+	// static int	getNbWithdrawals( void );
+	// static void	displayAccountsInfos( void );
+
+	// Account( int initial_deposit );
+	// ~Account( void );
+
+	// void	makeDeposit( int deposit );
+	// bool	makeWithdrawal( int withdrawal );
+	// int		checkAmount( void ) const;
+	// void	displayStatus( void ) const;
