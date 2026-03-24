40a1a0a8bfd5e67333c563a9fad18aa0968c4cff
Pedro Louzada <pedrolouzada17@gmail.com>
2026-03-24 14:47:40 +0000

    laptop to pc

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -16,5 +16,10 @@ int main(void)
 {
     std::string str = "HI THIS IS BRAIN";
 
-    
+    std::string* ptr = &str;
+    std::string& ref = str;
+
+    std::cout << &str << std::endl;
+    std::cout << &ptr << std::endl;
+    std::cout << &ref << std::endl;
 }
