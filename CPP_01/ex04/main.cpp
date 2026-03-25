e0656bb41659f55d7d2f424dda448f63f5e7c8a8
Pedro Louzada <pedrolouzada17@gmail.com>
2026-03-25 12:52:41 +0000

    04 done

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pbongiov <pbongiov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 22:12:15 by pbongiov          #+#    #+#             */
-/*   Updated: 2026/03/24 23:34:49 by pbongiov         ###   ########.fr       */
+/*   Updated: 2026/03/25 12:18:14 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,37 @@
 #include <fstream>
 #include <string>
 
+void replaceContent(std::ifstream& inFile, std::ofstream& outFile, std::string replace, std::string src)
+{
+    size_t pos;
+    std::string line;
+
+    while (std::getline(inFile, line))
+    {
+        while ((pos = line.find(replace)) != std::string::npos)
+        {
+            line.replace(pos, replace.size(), src.c_str());
+            pos += src.size(); 
+        }
+        outFile << line << "\n";
+    }
+    
+    outFile.close();
+    inFile.close();
+}
+
 int main(int ac, char **av)
 {
     (void)av;
     if (ac != 4)
         return (std::cerr << "Wrong number of arguments" << std::endl, 1);
-    
+        
     std::string fileName(av[1]);
     std::string s1(av[2]);
     std::string s2(av[3]);
 
+    if (fileName.empty() || s1.empty())
+        return (std::cerr << "FileName and s1 cannot be empty" << std::endl, 1);
     std::ifstream inFile(fileName.c_str());
     if (!inFile.is_open())
         return (std::cerr << "Error opening the file" << std::endl, 1);
@@ -33,8 +54,5 @@ int main(int ac, char **av)
     if (!outFile.is_open())
         return (std::cerr << "Error opening the file" << std::endl, 1);
     
-    outFile << inFile.rdbuf();
-    
-    std::string line;
-    while (std::getline(outFile, line))
+    replaceContent(inFile, outFile, s1, s2);
 }
