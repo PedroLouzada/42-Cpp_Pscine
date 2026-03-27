dde51ccf4b13ccfb41c378f570a325350e99e3b4
Pedro Louzada <pedrolouzada17@gmail.com>
2026-03-27 18:00:59 +0000

    Finished cpp01

---
+++ CPP_01/ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+    Harl harl;
+
+    harl.complain("DEBUG");
+    harl.complain("AAAAA");
+    harl.complain("ERROR");
+    harl.complain("info");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+}
---
+++ CPP_01/ex06/Harl.cpp
@@ -0,0 +1,60 @@
+#include "Harl.hpp"
+
+void Harl::debug(void)
+{
+    std::cout << "[DEBUG]\n";
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\nI really do!\n";
+}
+
+void Harl::warning(void)
+{   
+    std::cout << "[WARNING]\n";
+    std::cout << "I think I deserve to have some extra bacon for free.\nI've been coming for years, whereas you started working here just last month.\n";
+}
+
+void Harl::error(void)
+{
+    std::cout << "[ERROR]\n";
+    std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::info(void)
+{
+    std::cout << "[INFO]\n";
+    std::cout << "I cannot believe adding extra bacon costs more money.\nYou didn't put enough bacon in my burger!\nIf you did, I wouldn't be asking for more!\n";
+}
+
+int getIndex(std::string level)
+{
+    std::string array[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (array[i] == level)
+            return (i + 1);
+    }
+    return (0);
+}
+
+void Harl::complain(std::string level)
+{
+    int n = getIndex(level);
+
+    switch (n)
+    {
+        case 1:
+            this->debug();
+            // falls through
+        case 2:
+            this->info();
+            // falls through
+        case 3:
+            this->warning();
+            // falls through
+        case 4:
+            this->error();
+            break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]\n";
+    }
+}
